84413129e018f39b6a17ca9265039c6b89d39d46
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-08-15 17:49:42 -0300

    [feature] S3Service ajustada

--- src/main/java/br/com/dbccompany/colaboreapi/service/S3Service.java
+++ src/main/java/br/com/dbccompany/colaboreapi/service/S3Service.java
@@ -1,6 +1,8 @@
 package br.com.dbccompany.colaboreapi.service;
 
 import br.com.dbccompany.colaboreapi.exceptions.AmazonS3Exception;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -9,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 @Service
 @RequiredArgsConstructor
@@ -17,7 +20,7 @@ public class S3Service {
     @Value("${s3.bucket}")
     private String bucket;
 
-    /*private final AmazonS3 s3Client;
+    private final AmazonS3 s3Client;
 
     public URI uploadFile(MultipartFile multipartFile) throws AmazonS3Exception {
         try {
@@ -28,5 +31,17 @@ public class S3Service {
         } catch (IOException ex) {
             throw new AmazonS3Exception("Erro ao salvar a imagem!");
         }
-    }*/
+    }
+
+    public URI uploadFile (InputStream inputStream, String fileName, String contentType) throws AmazonS3Exception {
+        try{
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(contentType);
+            s3Client.putObject(bucket, fileName, inputStream, metadata);
+            return s3Client.getUrl(bucket, fileName).toURI();
+        } catch (URISyntaxException e) {
+            throw new AmazonS3Exception("Erro ao salva imagem!");
+        }
+    }
+
 }
