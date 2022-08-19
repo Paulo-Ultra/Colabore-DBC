242e4ed9a4f32d4a8fa1bb8c83474383a730a08d
Cesar <cesar.desenvolvedor@gmail.com>
2022-08-18 23:05:04 -0300

    correcao bug filtragem por tags query

--- src/main/java/br/com/dbccompany/colaboreapi/repository/CampanhaRepository.java
+++ src/main/java/br/com/dbccompany/colaboreapi/repository/CampanhaRepository.java
@@ -14,17 +14,20 @@ public interface CampanhaRepository extends JpaRepository<CampanhaEntity, Intege
    List<CampanhaEntity> findAllByIdUsuario(Integer idUsuario);
    List<CampanhaEntity> findAllByIdUsuarioAndIdCampanha(Integer idUsuario, Integer idCampanha);
 
-      @Query("select distinct c " +
-              " from campanha c" +
-              " left join c.doadores d " +
-              " left join c.tagEntities t " +
-              " where (:minhasContribuicoes = false OR d.idUsuario = :idUsuario )  " +
-              "   and (t.idTag in :idTag OR t.idTag <> null) " +
-              "   and (:minhasCampanhas = false OR c.idUsuario = :idUsuario) " +
-              " and (:statusMeta is null OR c.statusMeta = :statusMeta )" )
-      List<CampanhaEntity> findAll(@Param("statusMeta") Boolean statusMeta,
-                                   @Param("idUsuario")Integer idUsuario,
-                                   @Param("idTag")List<Integer> idTag,
-                                   @Param("minhasContribuicoes") Boolean minhasContribuicoes,
-                                   @Param("minhasCampanhas") Boolean minhasCampanhas);
+   @Query("select distinct c " +
+           " from campanha c" +
+           " left join c.doadores d " +
+           " left join c.tagEntities t " +
+           " where (:minhasContribuicoes = false OR d.idUsuario = :idUsuario )  " +
+           "   and (t.idTag in :idTag OR :idTag is null) " +
+           "   and (:minhasCampanhas = false OR c.idUsuario = :idUsuario) " +
+           " and (:statusMeta is null OR c.statusMeta = :statusMeta )" )
+   List<CampanhaEntity> findAll(@Param("statusMeta") Boolean statusMeta,
+                                @Param("idUsuario")Integer idUsuario,
+                                @Param("idTag")List<Integer> idTag,
+                                @Param("minhasContribuicoes") Boolean minhasContribuicoes,
+                                @Param("minhasCampanhas") Boolean minhasCampanhas);
+
+
+
 }
