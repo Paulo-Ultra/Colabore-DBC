073af5eb12c29fe5e8807c8e0a80340eea0cbc32
Cesar <cesar.desenvolvedor@gmail.com>
2022-08-21 16:17:19 -0300

    Documentação de Campanha Controller

--- src/main/java/br/com/dbccompany/colaboreapi/controller/CampanhaController.java
+++ src/main/java/br/com/dbccompany/colaboreapi/controller/CampanhaController.java
@@ -31,11 +31,13 @@ public class CampanhaController {
 
     private final CampanhaService campanhaService;
 
+    @Operation(summary = "Realiza o cadastro de uma campanha.", description = "Realiza o cadastro de uma campanha dentro do banco de dados.")
     @PostMapping("/cadastrar")
     public ResponseEntity<CampanhaDTO> cadastrar (@RequestBody @Valid CampanhaDTO campanhaCreateDTO) throws RegraDeNegocioException, CampanhaException {
         return new ResponseEntity<>(campanhaService.adicionar(campanhaCreateDTO), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Realiza o cadastro de uma foto para campanha.", description = "Realiza o cadastro de uma foto para campanha dentro do banco de dados.")
     @RequestMapping(
             path = "/cadastrarFoto",
             method = POST,
@@ -47,33 +49,33 @@ public class CampanhaController {
         return ResponseEntity.ok().build();
     }
 
-    @Operation(summary = "realiza a listagem de todas as campanhas do usuário logado", description = "lista de todas as campanhas do usuário logado no banco de dados")
+    @Operation(summary = "Realiza a listagem de todas as campanhas do usuário logado.", description = "Lista todas as campanhas presentes no banco de dados para o usuário logado.")
     @GetMapping("/listarCampanhasDoUsuario")
     public ResponseEntity<List<CampanhaDTO>> listarCampanhasDoUsuario() {
         return new ResponseEntity<>(campanhaService.listaDeCampanhasByUsuarioLogado(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Realiza a edição de uma determinada campanha.", description = "Executa a edição de uma campanha no banco de dados, através de seu respectivo id.")
     @PutMapping("/{idCampanha}")
     public ResponseEntity<CampanhaDTO> editar(@PathVariable("idCampanha") Integer idCampanha,
                                               @RequestBody @Valid CampanhaDTO campanhaDTO) throws CampanhaException, RegraDeNegocioException {
         return new ResponseEntity<>(campanhaService.editar(idCampanha, campanhaDTO), HttpStatus.OK);
     }
 
-    @Operation(summary = "realiza a deleção da campanha do usuário logado", description = "delete de campanha pelo identificador no banco de dados")
+    @Operation(summary = "Realiza a deleção da campanha do usuário logado.", description = "Delete de campanha pelo seu identificador no banco de dados.")
     @DeleteMapping("/delete")
     public ResponseEntity<Void> deleteCampanha(@RequestParam Integer id) throws CampanhaException, RegraDeNegocioException {
         campanhaService.deletar(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @Operation(summary = "realiza a listagem da campanha pelo id informado", description = "lista as informações da campanha pelo id no banco de dados")
+    @Operation(summary = "Realiza a listagem da campanha pelo id informado.", description = "Lista as informações da campanha pelo seu respectivo id.")
     @GetMapping("/campanhaPeloId")
     public ResponseEntity<CampanhaDTO> CampanhaPeloId(@RequestParam Integer idCampanha) throws CampanhaException {
         return new ResponseEntity<>(campanhaService.localizarCampanha(idCampanha), HttpStatus.OK);
     }
 
-    @Operation(summary = "realiza a listagem das campanhas com os status de atingida",
-            description = "lista as informações da campanha pelo status da meta como conclúídas no banco de dados")
+    @Operation(summary = "Realiza a listagem das campanhas através de determinados filtros.", description = "Recupera as campanhas presentes no banco de dados através de determinados filtros.")
     @GetMapping("/listarCampanhas")
     public ResponseEntity<List<CampanhaDTO>> listarCampanha(@RequestParam TipoFiltro tipoFiltro, @RequestParam boolean minhasContribuicoes, @RequestParam boolean minhasCampanhas, @RequestParam(required = false) List<Integer> idTags) throws RegraDeNegocioException, CampanhaException {
         return new ResponseEntity<>(campanhaService.listarCampanha(tipoFiltro, minhasContribuicoes, minhasCampanhas, idTags), HttpStatus.OK);
